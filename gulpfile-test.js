17096bc0c2d3392d1033067208a1c0c7c83bf950
SGS Venkatesh <venki.iitbbs@gmail.com>
2016-10-26 21:41:51 +0530

    Minor modifications

--- gulpfile-test.js
+++ gulpfile-test.js
@@ -15,6 +15,10 @@ gulp.task('build-scripts', function () {
 
 gulp.task('build-app', function () {
     return gulp.src(['app/**', 'index.html'], { base: "." }).pipe(gulp.dest('build'));
-})
+});
+
+gulp.task("watch-build", function () {
+    gulp.watch(['app/**'], ['build-app']);
+});
 
 gulp.task('build', ['build-app', 'build-scripts']);
