f02d390fc9322368d3eb2c6c6e09ff5fa7694839
SGS Venkatesh <venki.iitbbs@gmail.com>
2016-10-26 20:44:40 +0530

    Changes

--- gulpFile.js
+++ gulpFile.js
@@ -8,6 +8,13 @@ gulp.task('clean', function () {
     return gulp.src('build', { read: false }).pipe(clean());
 });
 
-gulp.task('build', function () {
-    return gulp.src(['app/**','index.html'], { base: "." }).pipe(gulp.dest('build'));
+gulp.task('build-scripts', function () {
+    gulp.src(config.vendorFiles.css).pipe(gulp.dest('build/lib/css'));
+    return gulp.src(config.vendorFiles.js).pipe(gulp.dest('build/lib/js'));
 });
+
+gulp.task('build-app', function () {
+    return gulp.src(['app/**', 'index.html'], { base: "." }).pipe(gulp.dest('build'));
+})
+
+gulp.task('build', ['build-app', 'build-scripts']);
